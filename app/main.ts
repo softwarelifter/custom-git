84cb32256ee16b084d0ff6ff05b8da64860d86a4
softwarelifter <softwarelifter@gmail.com>
2024-10-01 08:06:54 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -2,16 +2,25 @@ import * as fs from 'fs/promises';  // Change this line
 import zlib from 'zlib';
 import { join } from 'path';
 import { promisify } from 'util';
+import crypto from 'crypto'
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
     Init = "init",
-    Cat_File = "cat-file"
+    Cat_File = "cat-file",
+    Hash_Object = "hash-object"
 }
 
 const inflate = promisify(zlib.inflate);
+const deflate = promisify(zlib.deflate)
+
+const calculateSHA1 = (input: string) => {
+    const hash = crypto.createHash('sha1');
+    hash.update(input);
+    return hash.digest('hex');
+}
 
 const readObject = async (repoPath: string, sha: string): Promise<void> => {
     try {
@@ -55,6 +64,18 @@ const init = async (): Promise<void> => {
     }
 };
 
+const writeObject = async (contentFile: string) => {
+    const content = await fs.readFile(contentFile)
+    const size = content.length
+    const objectContent = `blob ${size}\0${content}`
+    const compressedData = await deflate(objectContent)
+    const sha1 = calculateSHA1(objectContent)
+    await fs.mkdir(`.git/objects/${sha1.slice(0, 2)}`, { recursive: true });
+    await fs.writeFile(`.git/objects/${sha1.slice(0, 2)}/${sha1.slice(2)}`, compressedData)
+
+    console.log(sha1)
+}
+
 const main = async (): Promise<void> => {
     switch (command) {
         case Commands.Init:
@@ -63,6 +84,11 @@ const main = async (): Promise<void> => {
         case Commands.Cat_File:
             await readObject(".git/objects", args[2]);
             break;
+        case Commands.Hash_Object:
+            if (args[1] == "-w")
+                await writeObject(args[2])
+            break;
+
         default:
             throw new Error(`Unknown command ${command}`);
     }
